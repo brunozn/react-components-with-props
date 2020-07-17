2c12308bf7292976dbd413a3b293aac4df1f2a04
brunozn <brunojndias@gmail.com>
2020-07-17 12:24:18 -0300

    Criando card

---
+++ src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./AnimalCard.css";
+
+export default function AnimalCard() {
+  return <h4>Animal</h4>;
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
 
+import data from "./data";
+import AnimalCard from "../AnimalCard/AnimalCard";
+
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <h1> Animals</h1>
+      {data.map((animal) => (
+        <AnimalCard key={animal.name} name={animal.name} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
