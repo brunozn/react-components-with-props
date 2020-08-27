2861a3bdcd834bceada7110dbbebca683d8d40c2
brunozn <brunojndias@gmail.com>
2020-08-27 03:03:20 -0300

    page login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,60 @@
 import React from 'react';
 
-import { Container } from './styles';
-import { ErrorMessage, Formik, Form, Field } from 'formik';
+import axios from 'axios';
+
+import {
+  Container,
+  TitleLogin,
+  FieldLogin,
+  ErrorMessageSpan,
+  ButtonLogin,
+} from './styles';
+import { Formik, Form } from 'formik';
+import * as yup from 'yup';
+
+//import { history } from '../../App/history';
 
 const Login = () => {
-  const handleSubmit = (values) => console.log(values);
+  const handleSubmit = (values) => {
+    axios.post('http://localhost:3001/v1/api/auth', values).then((resp) => {
+      const { data } = resp;
+      if (data) {
+        localStorage.setItem('app-token', data);
+        //history.push('/dashboard');
+      }
+    });
+  };
+
+  const validations = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required(),
+  });
+
   return (
     <Container>
-      <h1>Login</h1>
-      <Formik initialValues={{}} onSubmit={handleSubmit}>
+      <TitleLogin>Login</TitleLogin>
+      <Formik
+        initialValues={{}}
+        onSubmit={handleSubmit}
+        validationSchema={validations}
+      >
         <Form>
           <div>
-            <Field name="email" />
-            <ErrorMessage component="span" name="email"></ErrorMessage>
+            <FieldLogin name="email" type="email" />
+            <br></br>
+            <ErrorMessageSpan component="span" name="email" />
           </div>
-          <button type="submit">Entrar</button>
+
+          <div className="Login-Group">
+            <FieldLogin
+              name="password"
+              type="password"
+              className="Login-Field"
+            />
+            <br></br>
+            <ErrorMessageSpan component="span" name="password" />
+          </div>
+          <ButtonLogin type="submit">Entrar</ButtonLogin>
         </Form>
       </Formik>
     </Container>
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,8 +1,56 @@
 import styled from 'styled-components';
 
+import { Field, ErrorMessage } from 'formik';
+
 export const Container = styled.div`
-  background: #1db954;
-  width: 50vw;
-  margin: 0px auto;
+  background: #ffffff;
+  width: 30vw;
+  height: 40vh;
+  margin: calc(100vh / 3) auto 0px;
   text-align: center;
+`;
+
+export const TitleLogin = styled.h1`
+  color: #33333;
+  padding-top: 30px;
+`;
+
+export const FieldLogin = styled(Field)`
+  background: ##f5d5d5;
+  color: #333333;
+  margin: 0.5rem 0px;
+  padding: 0.2rem;
+  font-size: 1.2rem;
+  width: 20vw;
+
+  & focus {
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.5);
+    outline: none;
+  }
+`;
+
+export const ErrorMessageSpan = styled(ErrorMessage)`
+  color: #ff8080;
+`;
+
+export const ButtonLogin = styled.button`
+  color: #fff;
+  background-color: #1db954;
+  margin-top: 13px;
+  font-size: 14px;
+  line-height: 1;
+  border-radius: 500px;
+  padding: 16px 48px 18px;
+  transition-property: background-color, border-color, color, box-shadow, filter;
+  transition-duration: 0.3s;
+  border-width: 0;
+  letter-spacing: 2px;
+  min-width: 130px;
+  text-transform: uppercase;
+  white-space: normal;
+
+  &:hover {
+    cursor: pointer;
+    background: red;
+  }
 `;
